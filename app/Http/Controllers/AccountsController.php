694cbef5ea7480ff171b11f9ea0c51c0f5e6e194
nexus <danghoanglinh161994@gmail.com>
2017-12-26 02:35:16 +0700

    api

---
+++ app/Http/Controllers/AccountsController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Account;
+use App\User;
+use JWTAuth;
+use Input;
+
+class AccountsController extends Controller
+{
+	private $currentUser;
+	public function __construct(Request $request)
+	{
+		$this->currentUser = JWTAuth::toUser($request->token);
+	}
+    public function index(Request $request)
+    {
+        $accounts = $this->currentUser->account;
+        if ($accounts) {
+        	$status = 200;
+        } else {
+        	$status = 404;
+        }
+    	return response()->json([
+        	'status'=> $status,
+        	'data' => $accounts
+        ]);
+    }
+    public function show(Request $request, $id)
+    {
+        $account = $this->currentUser->account()->find($id);
+        if ($account) {
+        	$status = 200;
+        } else {
+        	$status = 404;
+        }
+        return response()->json([
+        	'status'=> $status,
+        	'data' => $account
+        ]);
+    }
+    public function save(Request $request)
+    {
+    	$account = $this->currentUser->account()->create(['email'=>$request->get('email'), 'password'=>$request->get('password')]);
+    	return response()->json([
+        	'status'=> 201,
+        	'data' => $account
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+    	$account = Account::find($id);
+    	if (!$account)
+        	$status = 404;
+        else {
+        	if ($this->currentUser->id == $account->user_id) {
+	        	$account->update(['email'=>$request->get('email'), 'password'=>$request->get('password')]);
+	        	$status = 200;
+	        } else {
+	        	$status = 401;
+	        }
+        }
+        return response()->json([
+        	'status'=> $status,
+        	'data' => $account
+        ]);
+    }
+    public function destroy(Request $request, $id)
+    {
+    	$account = Account::find($id);
+    	if (!$account)
+        	$status = 404;
+        else {
+        	if ($this->currentUser->id == $account->user_id) {
+	        	$account->delete();
+	        	$status = 200;
+	        } else {
+	        	$status = 401;
+	        }
+        }
+        return response()->json([
+        	'status'=> $status
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('auth/register', 'UserController@register');
+Route::post('auth/login', 'UserController@login');
+Route::group(['middleware' => 'jwt.auth'], function () {
+    Route::get('user-info', 'UserController@getUserInfo');
+	Route::get('/account/', 'AccountsController@index');
+	Route::get('/account/{id}', 'AccountsController@show');
+	Route::post('/account/', 'AccountsController@save');
+	Route::put('/account/{id}', 'AccountsController@update');
+	Route::delete('/account/{id}', 'AccountsController@destroy');
+});
